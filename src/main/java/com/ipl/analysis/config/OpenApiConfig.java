9399c5c1b378423a4004a57121db7e456b9ee2c5
Sujal124 <91372978+Divine124@users.noreply.github.com>
2026-04-10 05:42:44 +0530

    feat(swagger): add OpenApiConfig to enable JWT Authorize button in Swagger UI

---
+++ src/main/java/com/ipl/analysis/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.ipl.analysis.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "IPL Player Analysis API", version = "v1", description = "Endpoints for IPL backend"),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "Paste the JWT Token here!",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
